99af70223afbaf73ca93c5ea81a98995949dbca0
zereight <definedable@gmail.com>
2022-01-10 01:13:05 +0900

    chore: change root page

--- frontend(Next.js)/jaso-gpt/next.config.js
+++ frontend(Next.js)/jaso-gpt/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    async redirects() {
-      return [
-        { source: "/", destination: "/home", permanent: true }, // a permanent redirect
-      ];
-    },
+  exportPathMap: () => {
+    return { "/": { page: "/home" } };
   },
 };
