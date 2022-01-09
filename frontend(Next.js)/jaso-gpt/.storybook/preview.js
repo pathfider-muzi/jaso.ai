6f24f659da8558529de3d853d8b0abd4caff846d
zereight <definedable@gmail.com>
2022-01-09 16:55:36 +0900

    feat: apply global style
    - storybook too

--- frontend(Next.js)/jaso-gpt/.storybook/preview.js
+++ frontend(Next.js)/jaso-gpt/.storybook/preview.js
@@ -1,9 +1,20 @@
+import { Global } from "@emotion/react";
 import * as nextImage from "next/image";
+import globalStyles from "../styles/globalStyle";
 
 Object.defineProperty(nextImage, "default", {
   configurable: true,
   value: (props) => <img {...props} />,
 });
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global styles={globalStyles} />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
