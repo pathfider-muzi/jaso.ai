41d5117fda9972aa8f908df373ca7167acbdd63f
dobby.2(김정혁)/kakaobrain <dobby.2@kakaocorp.com>
2022-02-08 11:16:13 +0900

    feat: apply GA (#68)
    Co-authored-by: zereight <definedable@gmail.com>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,26 @@
 import globalStyles from "@/constants/styles/globalStyle";
 import wrapper from "@/modules/store";
+import * as gtag from "@/utils/gtag";
 import { Global } from "@emotion/react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import NextNProgress from "nextjs-progressbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageView(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <QueryClientProvider client={queryClient}>
--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,9 +1,27 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 const MyDocument = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_IDTRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
