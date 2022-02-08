16691a4f68b8cd797d4a01bfcf58d4f9faae9b49
dobby.2(김정혁)/kakaobrain <dobby.2@kakaocorp.com>
2022-02-08 11:44:24 +0900

    [FE] hotfix markup (#69)
    * refactor: remove unused file
    
    * fix: wrong style
    
    Co-authored-by: zereight <definedable@gmail.com>

--- frontend/constants/styles/verticalStyle.ts
+++ frontend/constants/styles/verticalStyle.ts
@@ -2,8 +2,6 @@ import { css } from "@emotion/react";
 
 const verticalStyle = css`
   display: flex;
-  margin-top: -20px;
-  margin-left: 80px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
