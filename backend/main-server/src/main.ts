6712c7788bfc342f3c8fed18261ae1c40f440358
eddy.storm(신우철)/kakaobrain <eddy.storm@kakaocorp.com>
2022-02-18 13:25:39 +0900

    [BE] cors policy rollback (#113)
    * fix: cors policy rollback
    
    * fix: changed port number
    
    Co-authored-by: eddy-storm <eddy.storm@kakaobrain.com>

--- backend/main-server/src/main.ts
+++ backend/main-server/src/main.ts
@@ -1,23 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const whitelist = ["http://jaso-ai.com/", "https://jaso-ai.com", "http://localhost:3000", "http://localhost:4000"];
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log(`Allowed CORS for: ${origin}`);
-        callback(null, true);
-      } else {
-        console.log(`Blocked cors for: ${origin}`);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: "GET,POST,PUT,PATCH,DELETE",
-    credentials: true
-  });
-  await app.listen(4000);
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
