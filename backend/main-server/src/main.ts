7394b9f910e02dc1341f2d65e5f5d07c207dcc2b
eddy.storm(신우철)/kakaobrain <eddy.storm@kakaocorp.com>
2022-02-18 13:05:18 +0900

    [BE] modified cors policy (#111)
    Co-authored-by: eddy-storm <eddy.storm@kakaobrain.com>

--- backend/main-server/src/main.ts
+++ backend/main-server/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const whitelist = ["/jaso-ai\.com/$", "http://localhost:3000", "http://localhost:4000"];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log(`Allowed CORS for: ${origin}`);
+        callback(null, true);
+      } else {
+        console.log(`Blocked cors for: ${origin}`);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: "GET,POST,PUT,PATCH,DELETE",
+    credentials: true
+  });
+  await app.listen(4000);
 }
 bootstrap();
