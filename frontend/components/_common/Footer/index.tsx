307aaacd4ad32b077a6192fede7c4a8d67ee6cfc
dobby.2(김정혁)/kakaobrain <dobby.2@kakaocorp.com>
2022-02-22 13:32:19 +0900

    fix: remove kakaobrain brand (#129)
    Co-authored-by: zereight <definedable@gmail.com>

--- frontend/components/_common/Footer/index.tsx
+++ frontend/components/_common/Footer/index.tsx
@@ -1,7 +1,7 @@
 import * as S from "./styles";
 
 const Footer = () => {
-  return <S.Frame>&copy; 2022 Team Muzi at KakaoBrain</S.Frame>;
+  return <S.Frame>&copy; 2022 Team Muzi</S.Frame>;
 };
 
 export default Footer;
