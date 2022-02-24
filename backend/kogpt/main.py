7a46b5741f26ec4ddfad8881871026a3609a2d14
jam.min(정종민)/kakaobrain <jam.min@kakaocorp.com>
2022-02-24 11:45:03 +0900

    [BE] KoGPT server - Message queue (#143)
    * feat: request to api server
    
    * feat: celery for message queue
    
    * chore: celery env
    
    * refactor: refactor pre-processing logic
    
    * feat: changed synchronous to asynchronous logic
    
    * feat: response multiple recommendations
    
    * fix: change response endpoint
    
    * feat: response queues
    
    * fix: adjust queueNum
    
    * fix: Roll back generation quantity
    
    * feat: add response key - resume id

--- backend/kogpt/main.py
+++ backend/kogpt/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 # import server dependencies
 from flask import Flask, request, jsonify
@@ -17,6 +18,9 @@ import motiveParser
 
 # Response validation check
 from responseValidChecker import stringValidation
+
+# Celery
+from celery import Celery
 
 # Log
 import logging
@@ -40,8 +44,29 @@ device = 'cuda'
 temperature = 0.8
 maxLength = 2048
 
+# Flask setting
 app = Flask(__name__)
+app.config.update(
+	CELERY_BROKER_URL='redis://localhost:6379',
+	CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
 CORS(app)
+
+# Celery setting
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+# Global var
+isInit = True
+model = None
+
+# get local json as prmompt string
+prompt_project_list = projectParser.parseLocalJsonToPromptString(filepath=prompt_project_json_path)
+motiveIdx, prompt_motive_list = motiveParser.parseLocalJsonToPromptString(cnt=0, filepath=prompt_motive_json_path)
+
+# response
+url = 'https://api-jasoai.kro.kr/'
+headers = {'Content-Type': 'application/json'}
 
 class KoGPTInference:
     def __init__(self, pretrained_model_name_or_path: Optional[Union[str, os.PathLike]], revision: str = 'KoGPT6B-ryan1.5b-float16', device: str = 'cuda'):
@@ -69,16 +94,50 @@ class KoGPTInference:
         gen_text = self.tokenizer.batch_decode(gen_tokens)[0]
         return gen_text
 
-# Start building KoGPT model
-print('Start building model')
-start = datetime.now()
-model = KoGPTInference(pretrained_model_name, revision, device=device)
-print(f'elapsed time for building model --> {datetime.now() - start}')
-# Finish building KoGPT model
+def init():
+    global isInit
+    global model
 
-# get local json as prmompt string
-prompt_project_list = projectParser.parseLocalJsonToPromptString(filepath=prompt_project_json_path)
-motiveIdx, prompt_motive_list = motiveParser.parseLocalJsonToPromptString(cnt=0, filepath=prompt_motive_json_path)
+    # Start building KoGPT model
+    print('Start building model')
+    start = datetime.now()
+    model = KoGPTInference(pretrained_model_name, revision, device=device)
+    print(f'elapsed time for building model --> {datetime.now() - start}')
+    # Finish building KoGPT model
+
+    isInit = False
+
+@celery.task
+def project_generation(d: dict, prompt: str, promptLen: int) -> str:
+    global isInit
+    if isInit:
+        init()
+    try:
+        recommendText = model.generate(prompt=prompt, temperature=temperature, max_length=maxLength)
+        recommendText = stringValidation(str(recommendText)[promptLen:])
+        logger.info(f'projectIntroduction: {recommendText}')
+        introductionByResume = {"resumeProjectId": d['resumeProjectId'], "projectIntroduction": recommendText}
+        res = requests.post(url=url + 'generation/project/result', json=introductionByResume, headers=headers).json()
+    except Exception as e:
+        exception_message = {"error": str(e)}
+        res = requests.post(url=url + 'generation/project/result', json=exception_message, headers=headers).json()
+    return res
+
+@celery.task
+def motive_generation(d: dict, prompt: str, promptLen: int) -> str:
+    global isInit
+    if isInit:
+        init()
+    try:
+        recommendText = model.generate(prompt=prompt, temperature=temperature, max_length=maxLength)
+        recommendText = stringValidation(str(recommendText)[promptLen:])
+        logger.info(f'motiveIntroduction: {recommendText}')
+        introductionByResume = {"resumeMotivationId": d['resumeMotivationId'], "motiveIntroduction": recommendText}
+        res = requests.post(url=url + 'generation/motivation/result', json=introductionByResume, headers=headers).json()
+    except Exception as e:
+        exception_message = {"error": str(e)}
+        res = requests.post(url=url + 'generation/motivation/result', json=exception_message, headers=headers).json()
+    return res
 
 @app.route("/project", methods=["POST"])
 def generate_project_self_introduction():
@@ -89,11 +148,9 @@ def generate_project_self_introduction():
         try:
             requestPrompt = projectParser.parseRequestJsonToPromptString(d=requestText)
             prompt, promptLen = projectParser.mergePrompts(local=prompt_project_list, req=requestPrompt)
-            recommendText = model.generate(prompt=prompt, temperature=temperature, max_length=maxLength)
-            recommendText = stringValidation(str(recommendText)[promptLen:])
-            logger.info(f'projectIntroduction: {recommendText}')
-            introductionByResume = {"projectIntroduction": recommendText}
-            return jsonify(introductionByResume)
+            task = project_generation.delay(d=requestText, prompt=prompt, promptLen=promptLen)
+            res = requests.get(url='http://localhost:5555/api/queues/length').json()['active_queues'][0]['messages']
+            return jsonify({"queueNum": res + 5})
         except Exception as e:
             return jsonify({"error": str(e)})
 
@@ -108,11 +165,9 @@ def generate_motive_self_introduction():
         try:
             requestPrompt = motiveParser.parseRequestJsonToPromptString(cnt=motiveIdx, d=requestText)
             prompt, promptLen = motiveParser.mergePrompts(local=prompt_motive_list, req=requestPrompt)
-            recommendText = model.generate(prompt=prompt, temperature=temperature, max_length=maxLength)
-            recommendText = stringValidation(str(recommendText)[promptLen:])
-            logger.info(f'motiveIntroduction: {recommendText}')
-            introductionByResume = {"motiveIntroduction": recommendText}
-            return jsonify(introductionByResume)
+            task = motive_generation.delay(d=requestText, prompt=prompt, promptLen=promptLen)
+            res = requests.get(url='http://localhost:5555/api/queues/length').json()['active_queues'][0]['messages']
+            return jsonify({"queueNum": res + 5})
         except Exception as e:
             return jsonify({"error": str(e)})
 
